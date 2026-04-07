0d0d243a526d5874544a34dd44250e40dd86360d
QAndreiCiciu <andreiciciu@gmail.com>
2026-04-07 15:30:07 +0300

    Am rezolvat

---
+++ Bomboclat Proiect/Assets/Scripts/Tile_Mattia.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using Random = System.Random;
+
+public class Tile_Mattia : MonoBehaviour
+{
+    private Tile _tempTile;
+    public int sceneType;
+    void Start()
+    {
+        generate();
+    }
+
+    private void generate()
+    {
+        var _scene = Resources.Load<SceneTemplate>($"Scene {sceneType}");
+        List<ListElem> x = _scene.elems;
+        for (int i = 0; i < x.Count; i++)
+        {
+            for (int j = x[i].x1; j <= x[i].x2; j++)
+            {
+                for (int k = x[i].y1; k <= x[i].y2; k++)
+                {
+                    _tempTile = GameObject.Find($"Tile {j} {k}").GetComponent<Tile>();
+                    _tempTile._sprite = x[i].blockSprite;
+                    _tempTile._coll = x[i].collide;
+
+                    _tempTile.Init();
+                }
+            }
+        }
+        
+    }
+}
