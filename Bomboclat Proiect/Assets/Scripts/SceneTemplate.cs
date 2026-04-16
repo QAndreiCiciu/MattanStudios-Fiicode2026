26d5a32d989840a998da3e01c342abafc863e1aa
ClarNuMattia <cojanmattia02@gmail.com>
2026-04-16 10:09:25 +0300

    idk ciciu le a facut

--- Bomboclat Proiect/Assets/Scripts/SceneTemplate.cs
+++ Bomboclat Proiect/Assets/Scripts/SceneTemplate.cs
@@ -6,5 +6,4 @@ using UnityEngine;
 public class SceneTemplate : ScriptableObject
 {
     public List<ListElem> elems;
-    public Sprite bgUsa;
 }
--- Bomboclat Proiect/Assets/Scripts/Tile_Mattia.cs
+++ Bomboclat Proiect/Assets/Scripts/Tile_Mattia.cs
@@ -30,6 +30,5 @@ public class Tile_Mattia : MonoBehaviour
                 }
             }
         }
-        
     }
 }
